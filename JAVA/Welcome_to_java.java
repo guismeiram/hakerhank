264bafee553ae4498086036eedecb846d4e24475
guismeiram <boralagui@outlook.com>
2022-07-13 15:46:59 -0300

    commitando primeiro exercicio java

---
+++ JAVA/Welcome_to_java.java
@@ -0,0 +1,8 @@
+package io.github.jiangdequan;
+
+public class Welcome_to_java {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
